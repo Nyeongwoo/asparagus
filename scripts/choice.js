941a1634cd0c5e550e088cb7097d4168e90587b2
Nyeongwoo <nnwoo@outlook.com>
2025-06-17 07:00:22 +0900

    Add files via upload

--- scripts/choice.js
+++ scripts/choice.js
@@ -1,9 +1,10 @@
-function addScoreAndNavigate(score, nextPage, gameoverPage = "gameover/default.html") {
-    let current = parseInt(localStorage.getItem("score")) || 0;
-    localStorage.setItem("score", current + score);
-
-    if (score === 0) {
-        location.href = gameoverPage;
+function addScoreAndNavigate(score, nextPage, gameOverPage = null) {
+    let currentScore = parseInt(sessionStorage.getItem('score') || '0');
+    currentScore += score;
+    sessionStorage.setItem('score', currentScore);
+    sessionStorage.setItem('lastScore', score); // 이전 선택 점수 저장
+    if (score === 0 && gameOverPage) {
+        location.href = gameOverPage;
     } else {
         location.href = nextPage;
     }
